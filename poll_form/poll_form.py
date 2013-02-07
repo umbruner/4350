142e09de87ff2c9d790fa5864ec74e6c072e3c6e
umvothr <umvothr@cc.umanitobca>
2013-02-06 21:25:40 -0600

    Revert "Added the ability to convert the input into JSON"
    This reverts commit 6b6bc805d03cad7f0b085516ab910de0f780e718.

--- poll_form/poll_form.py
+++ poll_form/poll_form.py
@@ -1,5 +1,4 @@
 def form():
-    import gluon.contrib.simplejson as sj
 
     form = FORM(TABLE(
         TR('Name:', INPUT(_type='text', _name='name', requires=IS_NOT_EMPTY())),
@@ -18,4 +17,4 @@ def form():
         response.flash = 'Please fill out the highlighted fields'
     else:
         response.flash = 'Please enter your poll information'
-    return dict(form=form, vars=sj.dumps(form.vars))
+    return dict(form=form, vars=form.vars)
